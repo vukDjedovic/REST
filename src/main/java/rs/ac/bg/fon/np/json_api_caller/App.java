8e786621a515fa10cf686e992996db407ae3a8c1
vukdjedovic <vukdjedovic98@gmail.com>
2022-04-01 22:22:07 +0200

    Napravljen je Maven projekat "json-api-caller" sa odgovarajucim parametrima i dodat je Maven dependency ka GSON 2.9.0 biblioteci.

---
+++ src/main/java/rs/ac/bg/fon/np/json_api_caller/App.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.np.json_api_caller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/rs/ac/bg/fon/np/json_api_caller/AppTest.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.np.json_api_caller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
